a308aeb5fb8588f32c24d193cc9c44e650265cc4
talhaunas1 <mtalha4600@gmail.com>
2023-02-23 14:42:28 +0500

    first push

---
+++ src/store/RootReducers.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name:"users",
+    initialState:{value: [] },
+    reducers:{
+        addUser:(state,action)=>{
+            
+        },
+        DeleteUser:(state,action)=>{
+
+        },
+        UpdateUser:(state,action)=>{
+
+        }
+    }
+})
+
+
+
+
+
+
